09c055352d228b891aec5609ee4b240879fa76b8
lostbitxxx <160622146+lostbitxxx@users.noreply.github.com>
2026-02-21 20:15:23 +0800

    init backend

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Backend running');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
